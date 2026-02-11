183f75e2d202f73929282669193ddfa2b1a709f1
Elif Altındaş <altindaselif@gmail.com>
2026-02-11 15:03:48 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const ratingPointButtons = document.querySelectorAll(".rating-point-button");
+const ratingSubmitButton = document.querySelector(".rating-submit-button");
+const ratingContainer = document.querySelector(".rating-container");
+const resultContainer = document.querySelector(".result-container");
+const resultRatingPoint = document.querySelector(".result-rating-point");
+
+let selectedRating = null;
+
+ratingPointButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    ratingPointButtons.forEach((button) => button.classList.remove("selected"));
+    e.currentTarget.classList.add("selected");
+
+    selectedRating = e.currentTarget.textContent;
+
+    ratingSubmitButton.removeAttribute("disabled");
+  });
+});
+
+ratingSubmitButton.addEventListener("click", () => {
+  resultRatingPoint.textContent = selectedRating;
+
+  ratingContainer.classList.add("hidden");
+  resultContainer.classList.remove("hidden");
+});
